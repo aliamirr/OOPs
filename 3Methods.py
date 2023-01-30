2c05444718e11c21d9fb43e6a3aaa0ad7d0f1f2b
Mohammed Aamir <aliamir@tutanota.com>
2023-01-30 11:56:33 +0530

    Complete Method

--- 3Methods.py
+++ 3Methods.py
@@ -1,3 +1,39 @@
+#just like function we can return values in methods, and they have access to the attributes of whatever instance they're bieng called on.
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.age = None
+
+
+    def say_hello(self):  #because methods acts on a instance of a class, self stores that instance, so you need self as the very first parameter.
+        print(f"Hello, {self.name}") # here I'm accessing the name attribute of whatever instance this method is being called on and I am printing it out.
+    
+    def set_age(self, age):
+        self.age = age #it is setting the value of an attribute. its taking age and it's then creating a new attribute called age and associating with whatever the age is, that we added in.
+
+    def get_age(self):
+        return self.age  #used mostly in other programming languages. #getter method is a method that gives you of an attribute. in python these are redundant as we can directly access the attribute directly by just referencing age.
+
+
+p1 = Person("Tim")
+p2 = Person("Susan")
+p1.set_age(21)
+p1.say_hello()
+p2.say_hello()
+print(p1.age)
+p1.set_age(24) #we can directly use p1.age(24)
+p1.get_age()
+
+
+
+
+
+
+
+
+
+
 class Counter:
     def __init__(self):
         self.count = 0
@@ -5,6 +41,7 @@ class Counter:
 
     def toggle_lock(self):
         self.locked = not self.locked
+        #or
         # if self.locked == True:
         #     self.locked = False
         # else:
@@ -26,7 +63,6 @@ class Counter:
 
 
 counter = Counter()
-# counter2 = Counter()
 
 
 counter.increment()
@@ -37,3 +73,52 @@ counter.print_count()
 
 counter.toggle_lock()
 counter.decrement()
+
+
+
+
+
+
+
+
+
+
+class Counter:
+    def __init__(self):
+        self.count = 0
+        self.locked = False
+
+    def toggle_lock(self):
+        self.locked = not self.locked
+        #or
+        # if self.locked == True:
+        #     self.locked = False
+        # else:
+        #     self.locked = True
+
+    def increment(self):
+        if self.locked:
+            raise Exception("The counter is locked!")
+        self.count += 1
+
+    def decrement(self):
+        if self.locked:
+            raise Exception("The counter is locked!")
+        self.count -= 1
+
+    def print_count(self):
+        print(f"The current count is {self.count}")
+
+
+
+counter = Counter()
+counter2 = Counter()
+
+
+counter.toggle_lock()
+counter2.increment()
+counter.print_count()
+counter2.print_count()
+
+
+
